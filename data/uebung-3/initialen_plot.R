65ada8a7351f84249e692d92f6bf02d01ba670bf
gwyssen <gerda.wyssen@proton.me>
2025-03-28 10:25:33 +0100

    fix images..  ... ...

---
+++ data/uebung-3/initialen_plot.R
@@ -0,0 +1,12 @@
+# Code innerhalb der folgenden 2 Linien darf nicht verändert werden
+# ---------------------------------------------------------------------
+library(tidyverse)
+d = read_csv('data/dataset_random_dot_clean.csv')
+# ---------------------------------------------------------------------
+
+# Beginnen Sie hier mit Ihrem Code:
+
+p = d |>
+    ggplot(...) +
+    ...
+p
